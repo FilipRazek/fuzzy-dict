2932feeefce353ced5c2fa2ed9875f93655edb66
Filip Razek <filip@razek.net>
2024-03-11 09:21:46 +0100

    :sparkles: Join hunchentoot at the end of main.lisp

--- backend/main.lisp
+++ backend/main.lisp
@@ -4,8 +4,17 @@
 (load "lev/package.lisp")
 (load "lev/compute.lisp")
 
-(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242 :document-root "/dev/null"))
-
 (hunchentoot:define-easy-handler (handle-levenshtein :uri "/levenshtein") (a b)
     (setf (hunchentoot:content-type*) "text/plain")
     (format nil "~A" (compute:levenshtein a b)))
+
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242 :document-root "/dev/null"))
+
+(handler-case
+  (let ((server-thread (find-if (lambda
+                                  (th)
+                                  (search "hunchentoot" (bt:thread-name th)))
+                                (bt:all-threads))))
+    (bt:join-thread server-thread))
+    (error (c) (format t "Woops, an unknown error occured:~&~a~&" c)))
+
