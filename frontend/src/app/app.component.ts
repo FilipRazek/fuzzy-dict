ca5468205efc6fbc5a33d61c2c721f52e8e027a3
Filip Razek <filip@razek.net>
2024-03-13 11:20:19 +0100

    :sparkles: Add first and second word variables to form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { ApiService } from './api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatInputModule],
+  imports: [RouterOutlet, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  firstWord = "";
+  secondWord = "";
   computedDistance = 0;
 
   constructor(private apiService: ApiService) { }
 
-  async ngOnInit() {
-    this.computedDistance = await this.apiService.fetchLevenshtein("kitten", "sitting");
+  recomputeDistance() {
+    this.apiService.fetchLevenshtein(this.firstWord, this.secondWord).then((distance) => {
+      this.computedDistance = distance;
+    });
   }
 }
