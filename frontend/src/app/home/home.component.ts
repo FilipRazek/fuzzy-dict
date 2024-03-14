4864ccf5f1c30cf3a07b027e49f7599df2824435
Filip Razek <filip@razek.net>
2024-03-14 09:00:06 +0100

    :sparkles: Add spinner when loading backend result

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ApiService } from '../api.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatInputModule, FormsModule],
+  imports: [MatInputModule, MatProgressSpinnerModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -14,12 +15,15 @@ export class HomeComponent {
   firstWord = "";
   secondWord = "";
   computedDistance = 0;
+  loading = false;
 
   constructor(private apiService: ApiService) { }
 
   recomputeDistance() {
+    this.loading = true;
     this.apiService.fetchLevenshtein(this.firstWord, this.secondWord).then(({ distance }) => {
       this.computedDistance = distance;
+      this.loading = false;
     });
   }
 }
